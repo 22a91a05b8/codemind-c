e20d23dd2a0c531bc9d060900d2802991b83565f
22a91a05b8 <119914388+22a91a05b8@users.noreply.github.com>
2022-12-28 18:47:10 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d",&a,&b);     c=a+b;     d=a-b;     printf("%d %d ",c,d);     float e,f,g,h;     scanf("%f%f",&e,&f);     g=e+f;     h=e-f;     printf("%.1f %.1f ",g,h); }

---
+++ Sum_and_Difference.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    d=a-b;
+    printf("%d %d
+",c,d);
+    float e,f,g,h;
+    scanf("%f%f",&e,&f);
+    g=e+f;
+    h=e-f;
+    printf("%.1f %.1f
+",g,h);
+}
